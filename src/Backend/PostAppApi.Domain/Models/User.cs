b3adb2737d48db74ddd3586bda697ba245e2e7f6
LSaints <mateus.dev@gmail.com>
2023-12-09 23:18:11 -0300

    Criado modelo de entitades para o banco de dados

---
+++ src/Backend/PostAppApi.Domain/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using PostAppApi.Domain.Common;
+
+namespace PostAppApi.Domain.Models
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<Post> Posts { get; } = new List<Post>();
+    }
+}
