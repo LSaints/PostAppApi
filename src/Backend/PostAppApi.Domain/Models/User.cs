2ae9873f7b86ec4699d0e2080d05fdfa4ccd895f
LSaints <mateus.dev@gmail.com>
2023-12-25 10:27:12 -0300

    fix: atributo de Posts agora pode ser nulo

--- src/Backend/PostAppApi.Domain/Models/User.cs
+++ src/Backend/PostAppApi.Domain/Models/User.cs
@@ -7,6 +7,6 @@ namespace PostAppApi.Domain.Models
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public ICollection<Post> Posts { get; } = new List<Post>();
+        public ICollection<Post>? Posts { get; } = new List<Post>();
     }
 }
