284a16b5e1593f59b25816d716c76de4da3414c1
LSaints <mateus.dev@gmail.com>
2023-12-25 10:29:13 -0300

    feat: Criado mappers de post

---
+++ src/Backend/PostAppApi.Application/Mappers/PostProfile/PostPostRequestBodyProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PostAppApi.Application.ModelViews.Post;
+using PostAppApi.Domain.Models;
+
+namespace PostAppApi.Application.Mappers.PostProfile
+{
+    public class PostPostRequestBodyProfile : Profile
+    {
+        public PostPostRequestBodyProfile() 
+        {
+            CreateMap<PostPostRequestBody, Post>();
+        }
+    }
+}
