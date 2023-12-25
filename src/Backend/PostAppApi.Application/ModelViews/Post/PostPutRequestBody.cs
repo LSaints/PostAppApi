acf9268c05be60843a81051d5a0c8bef1a25f15d
LSaints <mateus.dev@gmail.com>
2023-12-25 10:27:43 -0300

    Criado ModelView para Post

---
+++ src/Backend/PostAppApi.Application/ModelViews/Post/PostPutRequestBody.cs
@@ -0,0 +1,11 @@
+﻿using PostAppApi.Application.ModelViews.Common;
+
+namespace PostAppApi.Application.ModelViews.Post
+{
+    public class PostPutRequestBody : BasePutEntity
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int UserId { get; set; }
+    }
+}
