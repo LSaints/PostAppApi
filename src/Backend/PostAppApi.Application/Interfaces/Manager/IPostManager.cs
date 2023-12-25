21677614c581dafc9c537c78ee684c6292e417e8
LSaints <mateus.dev@gmail.com>
2023-12-25 10:29:47 -0300

    feat: Adicionado interfaces de posts

---
+++ src/Backend/PostAppApi.Application/Interfaces/Manager/IPostManager.cs
@@ -0,0 +1,13 @@
+﻿using PostAppApi.Application.Interfaces.Commons;
+using PostAppApi.Application.ModelViews.Post;
+using PostAppApi.Application.ModelViews.User;
+using PostAppApi.Domain.Models;
+
+namespace PostAppApi.Application.Interfaces.Manager
+{
+    public interface IPostManager : IManager<Post>
+    {
+        public Task<Post> InsertAsync(PostPostRequestBody entity);
+        public Task<Post> UpdateAsync(PostPutRequestBody entity);
+    }
+}
