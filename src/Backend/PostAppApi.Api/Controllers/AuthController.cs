740c1bde8862caba0a13ec4c2a0a9f25e60a4382
LSaints <mateus.dev@gmail.com>
2023-12-29 18:58:08 -0300

    refactor: Alterado caminho das modelviews

---
+++ src/Backend/PostAppApi.Api/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostAppApi.Application.ModelViews.User;
+
+namespace PostAppApi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Login()
+        {
+            return Ok("Login realizado com sucesso.");
+        }
+    }
+}
