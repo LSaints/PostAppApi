447e749a3c77cfa2296da3a696592f0c42d1b63c
lsants <lsants.dev@gmail.com>
2024-02-13 14:07:11 -0300

    feat: Adicionar Serilog

---
+++ src/Backend/PostAppApi.Api/Configuration/SerilogExtension.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace PostAppApi.Api.Configuration
+{
+    public static class SerilogExtension
+    {
+        public static void AddSerilogApi(this IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithMachineName()
+                .Enrich.WithExceptionDetails()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code, outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+        }
+    }
+}
