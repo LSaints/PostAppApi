78eb35fe6f5e197b9bdf6fc5f989bd38b07b4bbb
lsants <lsants.dev@gmail.com>
2024-02-15 13:26:34 -0300

    feat: Criar classes de exceções personalizadas para posts

---
+++ src/Shared/PostAppApi.Exceptions/PostExceptions/UserNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using PostAppApi.Exceptions.Messages;
+
+namespace PostAppApi.Exceptions.PostExceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException() : base(PostMessagesExceptions.USER_NOT_FOUND_EXCEPTION) { }
+
+        public UserNotFoundException(string message) : base(message) { }
+    }
+}
